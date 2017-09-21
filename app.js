c6520933dc5c0b9a05f4f8c00b5e96cadf8e6610
Guihgo <guihgo100milha@gmail.com>
2017-09-20 22:06:53 -0300

    agora ok

--- app.js
+++ app.js
@@ -9,8 +9,7 @@ var app = express();
 var server = require('http').createServer(app); //cria servidor http utilizando express como intermediador
 
 server.listen(process.env.PORT || 80); //inicia servidor http na porta 80
-var socketIo = io.listen(server); //inicia servidor socket utilizando o servidor http criado
-console.log('Server http e socket rodando...');
+console.log('Server http  rodando...');
 
 connections = []; //array que guardar as conexões
 var msgs = []; //array que guarda as mensagens
@@ -19,6 +18,4 @@ app.use(express.static(path.join(__dirname, 'public'))); //deixa public a pasta 
 //Rota para o caminho : 127.0.0.1:80/
 app.get('/', function(req, res) { //quando houver requisição nesse caminho
 	res.sendFile(path.join(__dirname, 'index.html')); //envia essa página como resposta
-});
-
 });
